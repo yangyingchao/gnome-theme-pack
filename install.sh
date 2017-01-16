008c4b11142a485a32253cc650187506df6d5e4c
yangyingchao <yangyingchao@gmail.com>
2017-01-16 10:29:11 +0800

    added install.sh.

---
+++ install.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Author: Yang,Ying-chao <yangyingchao@gmail.com>, 2017-01-16
+#
+
+die() {
+    set +x
+    local IFS=$' \t\n'
+    set +e
+
+    echo -e "$@" | while read -r ; do
+        echo " $BAD*$NORMAL $RC_INDENTATION$REPLY" >&2
+    done
+    exit 1
+}
+
+
+if [ -f "gnome-terminal/gnome-terminal-theme.sh" ]; then
+    bash gnome-terminal/gnome-terminal-theme.sh
+    if [ $? -ne 0 ]; then
+        die "failed to install gnome-terminal-theme."
+    fi
+else
+    die "gnome-terminal-theme.sh is missing"
+fi
+
+
+cd numix-gtk-theme/ && sudo make install
+
+cd extensions
+
+cd pixel-saver
+
+[ -f ~/.local/share/gnome-shell/extensions/pixel-saver@deadalnix.me ] ||  ln -sf ${PWD}/pixel-saver@deadalnix.me ~/.local/share/gnome-shell/extensions/
